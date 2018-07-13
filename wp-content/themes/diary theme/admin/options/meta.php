1e29ae7a72749bc42ebae7ee8b369282bd6add36
sumaya87 <sumaya87@gmail.com>
2018-07-14 04:25:50 +1000

    changed meta description

--- wp-content/themes/diary theme/admin/options/meta.php
+++ wp-content/themes/diary theme/admin/options/meta.php
@@ -5,11 +5,11 @@
 
     $options[] = array( "name" => "Meta Description",
 						"id" => $shortname."_description",
-						"std" => "full functionable, premium wordpress theme solution for your website.",
+						"std" => "",
 						"type" => "textarea");
 
     $options[] = array( "name" => "Meta Keywords",
-						"std" => "proffesional wordpress theme, flexible wordpress theme, wordpress all in one theme, premium wordpress theme ",
+						"std" => "professional wordpress theme, flexible wordpress theme, wordpress all in one theme, premium wordpress theme ",
 						"id" => $shortname."_keywords",
                         "type" => "textarea");
 ?>
